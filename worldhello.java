771ec3eb31347c8660250a5e6df0ff1b40450cff
Subrahmanyam Chavali <chsubbu91@gmail.com>
2016-10-21 13:10:25 +0530

    test2

--- worldhello.java
+++ worldhello.java
@@ -7,7 +7,7 @@ public class HelloWorld {
    public static void main(String[] args) {
          // Prints "Hello, World" in the terminal window.
 	       System.out.println("Hello, World");
-		   System.out.println("Hi this good");
+		   System.out.println("Hi this first good");
 		   System.out.println("Hi this secound good");
 		   
 	          }
